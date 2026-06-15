508f2f0c258ac83de4c4bf579b00c2f074d89fc2
ah <me@hellings.cc>
2026-06-15 18:57:58 -0400

    Fix inputs

--- build.rs
+++ build.rs
@@ -1,16 +1,17 @@
 fn main() {
     if std::env::var("CARGO_FEATURE_QT").is_ok() {
-        if let Ok(libs) = pkg_config::Config::new().probe("Qt6Core") {
-            for lib in &libs.libs {
-                println!("cargo:rustc-link-lib={}", lib);
-            }
-            for path in &libs.link_paths {
-                println!("cargo:rustc-link-search={}", path.display());
-            }
+        let libs = pkg_config::Config::new().probe("Qt6Core").unwrap();
+        for lib in &libs.libs {
+            println!("cargo:rustc-link-lib={}", lib);
         }
-        cc::Build::new()
-            .cpp(true)
-            .file("src/fdf_bridge.cpp")
-            .compile("fdf_bridge");
+        for path in &libs.link_paths {
+            println!("cargo:rustc-link-search={}", path.display());
+        }
+        let mut build = cc::Build::new();
+        build.cpp(true).file("src/fdf_bridge.cpp");
+        for path in &libs.include_paths {
+            build.include(path);
+        }
+        build.compile("fdf_bridge");
     }
 }
